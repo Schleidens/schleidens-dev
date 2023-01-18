e9c5597f69c79dd7f4580fae8b52feb2209f72df
Schleidens <alectineschleidens2016@gmail.com>
2023-01-17 23:33:49 -0500

    add tailwind.config and config custom color names

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
       colors: {
         'jaune': '#fcba28',
         'smocky-black': '#0F0D0E',
+        'happy-black': '#2e2a2a'
         'raisin-black': '#231F20',
         'cornsilk': '#F9F4DA',
         'pink': '#F38BA3',
