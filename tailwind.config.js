7f70b008ce6ff1d30483f660e226c59337f44063
Schleidens <alectineschleidens2016@gmail.com>
2023-01-17 23:33:49 -0500

    add tailwind.config and config custom color names

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        'jaune': '#fcba28',
+        'smocky-black': '#0F0D0E',
+        'raisin-black': '#231F20',
+        'cornsilk': '#F9F4DA',
+        'pink': '#F38BA3',
+        'purple': '#7B5EA7',
+      }
+    }
+  }
+}
