1def3af16a2d10b50d4d50e53dcc7e21266804e2
Schleidens <alectineschleidens2016@gmail.com>
2023-01-18 23:37:13 -0500

    add link and heart emoji

--- components/theFooter.vue
+++ components/theFooter.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex justify-center">
-        Build with heart by Schleidens.Dev
+    <div class="flex justify-center text-cornsilk text-xl">
+        Build with &#9829; by <a class="underline decoration-solid ml-2" href="https://github.com/Schleidens">Schleidens.Dev</a>
     </div>
 </template>
 
