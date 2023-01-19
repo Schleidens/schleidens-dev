c9c601d5674f42b9545077572bd0c9d72cb92358
Schleidens <alectineschleidens2016@gmail.com>
2023-01-19 12:23:13 -0500

    create and render the intro

---
+++ components/intro.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-raisin-black shadow py-10 md:py-20 px-2 text-left text-3xl md:text-5xl">
+        <h2 class="text-5xl text-jaune">
+            &#128075; Hi there, I'm Schleidens.
+        </h2>
+        I am a self-taught fullstack web developer with a passion for creating dynamic and engaging web experiences.
+        My interest in programming began as a hobby and quickly turned into a career.
+        I am constantly learning and expanding my skill set to stay up-to-date with the latest industry trends and best practices.
+        <div class="text-xl text-pink">
+            I like contribute to open source projects and I am open to new collaborations and opportunities.        
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
