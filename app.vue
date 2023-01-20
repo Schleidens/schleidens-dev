f6c5478b55ab7771c05aebcf31f9a14cdcfeff8e
Schleidens <alectineschleidens2016@gmail.com>
2023-01-20 12:48:57 -0500

    set meta tags and add image for favicon

--- app.vue
+++ app.vue
@@ -8,5 +8,24 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+  const title = ref("Schleidens.Dev - Fullstack web developer")
+  const description = ref("I am a self-taught fullstack web developer with a passion for creating dynamic and engaging web experiences.")
+  const image = ref("https://res.cloudinary.com/schleidens/image/upload/v1674236203/cover_image_1schleidens_u2wspj.png")
+
+  useHead({
+  link: [{ rel: 'icon', type: 'image/png', href: 'favicon.png' }],
+  title: title,
+  meta: [
+    { name: 'description', content: description },
+    { property: 'og:title', content: title},
+    { property: 'og:description', content: description},
+    { property: 'og:image', content: image},
+    { name: 'twitter:card', content: 'summary'},
+    { name: 'twitter:site', content: '@Schleidens_dev'},
+    { name: 'twitter:title', content: title},
+    { name: 'twitter:description', content: description},
+    { name: 'twitter:image', content: image}
+  ],
+})
 </script>
