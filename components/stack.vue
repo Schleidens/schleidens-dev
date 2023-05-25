6438bbfff3ea2ec00d14608aa6ddbb9eb2ee65a7
Schleidens <alectineschleidens2016@gmail.com>
2023-05-25 12:45:01 -0400

    update my stacks

--- components/stack.vue
+++ components/stack.vue
@@ -2,16 +2,40 @@
     <div class="bg-pink text-raisin-black shadow mt-6 py-10 px-4 text-left">
         <h2 class="text-4xl font-black underline">myStack();</h2>
         <div class="text-xl">
-            <p>I spend most of my energy building apps with <span class="font-black">MEVN</span> && <span class="font-black">MERN</span> technology</p>
+            <p>I spend most of my energy building stuffs with <span class="font-black">Django | Vue.js | React.js</span></p>
             <div class="mt-2">
                 <span class="text-2xl font-black">My stack includes the following technologies:</span>
-                <p class="">ReactJs, Vue.Js, NuxtJS, NextJS, GatsbyJs, NodeJS, ExpressJS, RestAPI, GraphQL, MongoDB</p>
+                
+                <div class="mt-2" v-for="{title, stack} in stacks">
+                    <div class="font-black">
+                        {{ title }}
+                    </div>
+                    <div>
+                        {{ stack }}
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+
+    const stacks = ref([
+        {
+            title: "Languages",
+            stack: "Python | Javascript |  Typescript"
+        },
+        {
+            title: "Front End",
+            stack: "Vue.js | React.js | Nuxt.js | Next.js | Gatsby | Vite | TailwindCss | Bootstrap"
+        },
+        {
+            title: "Back End",
+            stack: "Django | Nodejs | Express.js | Headless CMS | MongoDB | PostgreSQL"
+        }
+    ])
 </script>
 
 <style scoped>
