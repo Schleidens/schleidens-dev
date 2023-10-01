72c866393f9f950a9b2dbaed74878ea590f33a62
Schleidens <alectineschleidens2016@gmail.com>
2023-10-01 13:42:27 -0400

    install tailwind typography and set it and add params for @nuxt/content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        'jaune': '#fcba28',
+        jaune: '#fcba28',
         'smocky-black': '#0F0D0E',
         'happy-black': '#2e2a2a',
         'raisin-black': '#231F20',
-        'cornsilk': '#F9F4DA',
-        'pink': '#F38BA3',
-        'purple': '#7B5EA7',
-      }
-    }
-  }
-}
+        cornsilk: '#F9F4DA',
+        pink: '#F38BA3',
+        purple: '#7B5EA7',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+};
