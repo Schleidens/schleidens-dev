f4627b290244518e267b60a0bad1e80e79410ecf
Schleidens <alectineschleidens2016@gmail.com>
2024-03-23 23:14:59 -0400

    add more skills in stack

--- components/stack.vue
+++ components/stack.vue
@@ -1,42 +1,52 @@
 <template>
-    <div class="bg-pink text-raisin-black shadow mt-6 py-10 px-4 text-left">
-        <h2 class="text-4xl font-black underline">myStack();</h2>
-        <div class="text-xl">
-            <p>I spend most of my energy building stuffs with <span class="font-black">Django | Vue.js | React.js</span></p>
-            <div class="mt-2">
-                <span class="text-2xl font-black">My stack includes the following technologies:</span>
-                
-                <div class="mt-2" v-for="{title, stack} in stacks">
-                    <div class="font-black">
-                        {{ title }}
-                    </div>
-                    <div>
-                        {{ stack }}
-                    </div>
-                </div>
+  <div class="bg-pink text-raisin-black shadow mt-6 py-10 px-4 text-left">
+    <h2 class="text-4xl font-black underline">myStack();</h2>
+    <div class="text-xl">
+      <p>
+        I spend most of my energy building stuffs with
+        <span class="font-black"
+          >React.js | Node/Express | Postgres | typeORM | Prisma | Websocket |
+          Firebase</span
+        >
+      </p>
+      <div class="mt-2">
+        <span class="text-2xl font-black"
+          >My stack includes the following technologies:</span
+        >
 
-            </div>
+        <div
+          class="mt-2"
+          v-for="{ title, stack } in stacks"
+        >
+          <div class="font-black">
+            {{ title }}
+          </div>
+          <div>
+            {{ stack }}
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-
-    const stacks = ref([
-        {
-            title: "Languages",
-            stack: "Python | Javascript | Typescript"
-        },
-        {
-            title: "Front End",
-            stack: "Vue.js | React.js | Nuxt.js | Next.js | Gatsby | Vite | TailwindCss | Bootstrap | SCSS"
-        },
-        {
-            title: "Back End",
-            stack: "Django | Nodejs | Express.js | Headless CMS | MongoDB | PostgreSQL | Firebase | Supabase"
-        }
-    ])
+  const stacks = ref([
+    {
+      title: 'Languages',
+      stack: 'Python | Javascript | Typescript',
+    },
+    {
+      title: 'Front End',
+      stack:
+        'Vue.js | React.js | Nuxt.js | Next.js | Gatsby | Vite | TailwindCss | Bootstrap | SCSS',
+    },
+    {
+      title: 'Back End',
+      stack:
+        'Django | Nodejs | Express.js | Headless CMS | MongoDB | PostgreSQL | TypeORM | Prisma | Firebase | Supabase',
+    },
+  ]);
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
